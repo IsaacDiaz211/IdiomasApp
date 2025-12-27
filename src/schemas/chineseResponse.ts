b014b11dd1b6f77e5d5e051b5873f2c54cc53a4d
Isaac Diaz <isaac211.md@outlook.com>
2025-12-27 01:10:30 -0300

    add chinese

---
+++ src/schemas/chineseResponse.ts
@@ -0,0 +1,34 @@
+import { t, Static } from 'elysia';
+import { z } from 'zod';
+
+const GlossedChineseZodSchema = z.object({
+  separateWords: z.array(z.string()),
+  pinyin: z.array(z.string()),
+  glossedWords: z.array(z.string())
+});
+
+export { GlossedChineseZodSchema };
+
+const GlossedChineseSentenceSchema = t.Transform(
+  t.Object({
+    separateWords: t.Array(t.String()),
+    pinyin: t.Array(t.String()),
+    glossedWords: t.Array(t.String())
+  })
+)
+.Decode(data => {
+    if (data.separateWords.length !== data.glossedWords.length && data.separateWords.length !== data.pinyin.length) {
+        throw new Error("separateWords, pinyin, and glossedWords must have the same length");
+    }
+    return data;
+}).Encode(data => data);
+
+export type GlossedChineseSentence = Static<typeof GlossedChineseSentenceSchema>;
+ 
+export const ChineseResponseSchema = t.Object({
+  request_id: t.String({ format: 'uuid' }),
+  translatedText: t.String(),
+  glossedText: t.Array(GlossedChineseSentenceSchema),
+});
+
+export type ChineseResponse = Static<typeof ChineseResponseSchema>;
