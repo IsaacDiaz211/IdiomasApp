badbe6bb056945882f4989f9da565f4f7da8362d
Isaac Diaz <isaac211.md@outlook.com>
2026-02-06 17:29:24 -0300

    minor changes

--- src/TranslationController.ts
+++ src/TranslationController.ts
@@ -8,9 +8,6 @@ const TranslationController = new Elysia()
     .post(
         '/translate/:grammar?',
         async ({ body, params: { grammar } }) => {
-            if((body.l2 === 'ko' && body.l1 !== 'en') || (body.l2 !== 'en' && body.l1 === 'ko')) {
-                throw new Error("Korean (ko) translations are only supported with English (en) as the other language.");
-            }
             let translationResult;
             if (grammar){
                 translationResult = await runTranslationPipeline(body, true, true);
